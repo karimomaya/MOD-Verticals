e1829371e5730f3febe6c30d09ccb2572ce14808
karim.omaya <karim.omaya.elfaham@gmail.com>
2020-01-08 11:39:04 +0200

    Update Report Controller

--- js-code/TaskManagement/pages/stored-procedures/MOD_TM_SP_GetTasksByPerformerId.sql
+++ js-code/TaskManagement/pages/stored-procedures/MOD_TM_SP_GetTasksByPerformerId.sql
@@ -1,6 +1,6 @@
 USE [awdb]
 GO
-/****** Object:  StoredProcedure [dbo].[MOD_TM_SP_FinishedAndDelayedTaskReportProject]    Script Date: 1/6/2020 12:19:48 PM ******/
+/****** Object:  StoredProcedure [dbo].[MOD_TM_SP_GetTasksByPerformerId]    Script Date: 1/8/2020 11:07:28 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -10,20 +10,24 @@ GO
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-CREATE PROCEDURE [dbo].[MOD_TM_SP_GetTasksByPerformerId]
+ALTER PROCEDURE [dbo].[MOD_TM_SP_GetTasksByPerformerId]
 	-- Add the parameters for the stored procedure here
-	@performerId int
+	@performerId int,
+	@StartDate date,
+	@EndDate date
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-SELECT task.* FROM O2MyCompanyTaskManagementMOD_TM_entity_Task  as task 
+SELECT task.Id, task.startDate, task.dueDate, task.status as taskStatus, task.taskName, targetTask.status as targetPerformerStatus, person.DisplayName  FROM O2MyCompanyTaskManagementMOD_TM_entity_Task  as task 
 INNER JOIN O2MyCompanyTaskManagementMOD_TM_entity_TaskPerformer as performer on performer.taskPerformerToTask_Id = task.Id
 INNER JOIN O2MyCompanyTaskManagementMOD_TM_entity_TargetTaskPerformer as targetTask on targetTask.targetTaskPerformerToTaskPerformer_Id = performer.Id
+inner join O2OpenTextEntityIdentityComponentsIdentity as iden on iden.Id =targetTask.performerId
+	inner join O2OpenTextEntityIdentityComponentsPerson as person on iden.toPerson_Id = person.Id
 where 
 targetTask.performerId = @performerId
-and  (task.status = 1 or task.status = 2 ) and task.isDeleted <> 1
+and  (task.status = 1 or task.status = 2 or task.status = 3 ) and task.isDeleted <> 1 and task.startDate >= @StartDate and task.dueDate <=@EndDate and (targetTask.status <> 4 or targetTask.status <> 3) 
 
 END
