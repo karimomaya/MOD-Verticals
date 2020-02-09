2b819008f487d9b9ff6a4a8781f3fb27d2762085
Karim <karim.omaya.elfaham@gmail.com>
2020-02-09 16:20:22 +0200

    Update Security Request

--- mod-soap/src/main/java/com/mod/soap/model/SecurityRequest.java
+++ mod-soap/src/main/java/com/mod/soap/model/SecurityRequest.java
@@ -1,11 +1,20 @@
 package com.mod.soap.model;
 
+import javax.xml.bind.annotation.*;
+
 /**
  * Created by karim on 2/8/20.
  */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+        "samlart", "type", "target"
+})
 public class SecurityRequest {
+    @XmlElement(name = "samlart", required = false)
     String samlart;
+    @XmlElement(name = "type", required = false)
     int type;
+    @XmlElement(name = "target", required = false)
     String target;
 
 
