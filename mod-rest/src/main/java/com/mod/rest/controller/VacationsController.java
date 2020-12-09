4f16d25486cdb1ab8ef17145fce8440bf679bdb0
Omar Sabry <omar.adell96@gmail.com>
2020-12-09 23:09:29 +0200

    Get Vacations within date

--- mod-rest/src/main/java/com/mod/rest/controller/VacationsController.java
+++ mod-rest/src/main/java/com/mod/rest/controller/VacationsController.java
@@ -13,7 +13,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -30,31 +33,81 @@ public class VacationsController {
     @GetMapping("get/{startDate}/{endDate}")
     @ResponseBody
     public ResponseBuilder<ObjectNode> getAll(
-            @PathVariable("startDate") String startDate,
-            @PathVariable("endDate") String endDate) {
+            @PathVariable("startDate") String startDateString,
+            @PathVariable("endDate") String endDateString) {
 
         ResponseBuilder<ObjectNode> responseBuilder = new ResponseBuilder<>();
         ObjectMapper mapper = new ObjectMapper();
         ObjectNode result = mapper.createObjectNode();
+        try {
+            List<Vacation> generalList = vacationRepository.findByRule("general");
+            List<Vacation> exceptionList = vacationRepository.findByRule("exception");
 
-        Iterable<Vacation> vacationList = vacationRepository.findAll();
-        List<Vacation> resultVacations = new ArrayList<>();
-        for (Vacation vacation: vacationList) {
-//            if(vacation.getRule().equals("general")){
-//
-//            }else{
-//
-//            }
-            resultVacations.add(vacation);
-        }
-        if(resultVacations.size() > 0){
-            result.put("vacations", Utils.writeObjectIntoString(resultVacations));
-            responseBuilder.status(ResponseCode.SUCCESS);
-        }
-        else {
-            responseBuilder.status(ResponseCode.NO_CONTENT);
-        }
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            Date startDate = simpleDateFormat.parse(startDateString);
+            Date endDate = simpleDateFormat.parse(endDateString);
 
+            List<Vacation> resultVacations = new ArrayList<>();
+            for (Vacation exception : exceptionList) {
+                Date fromDate = simpleDateFormat.parse(exception.getFromDate());
+                Date toDate = simpleDateFormat.parse(exception.getToDate());
+                if (Utils.isDateBetweenDates(fromDate,startDate,endDate)
+                        || Utils.isDateBetweenDates(toDate,startDate,endDate)) {
+                    resultVacations.add(exception);
+                }
+            }
+            for (Vacation general : generalList) {
+                String year = startDateString.split("-")[0];
+
+                String fromDateString = general.getFromDate();
+                String toDateString = general.getToDate();
+
+                String fromDateYear = fromDateString.substring(0,fromDateString.indexOf("-"));
+                String toDateYear = toDateString.substring(0,toDateString.indexOf("-"));
+
+                fromDateString = year + fromDateString.substring(fromDateString.indexOf("-"),fromDateString.length());
+
+                if(fromDateYear.equals(toDateYear)){
+                    toDateString = year + toDateString.substring(toDateString.indexOf("-"),toDateString.length());
+                }else{
+                    int yearInt = Integer.parseInt(year);
+                    yearInt += 1;
+                    toDateString = yearInt + toDateString.substring(toDateString.indexOf("-"),toDateString.length());
+                }
+
+                Date fromDate = simpleDateFormat.parse(fromDateString);
+                Date toDate = simpleDateFormat.parse(toDateString);
+
+                if (Utils.isDateBetweenDates(fromDate,startDate,endDate)
+                        || Utils.isDateBetweenDates(toDate,startDate,endDate)) {
+                    if(!hasExceptionInList(general, resultVacations)){
+                        general.setFromDate(fromDate);
+                        general.setToDate(toDate);
+                        resultVacations.add(general);
+                    }
+                }
+            }
+
+            if (resultVacations.size() > 0) {
+                result.put("vacations", Utils.writeObjectIntoString(resultVacations));
+                responseBuilder.status(ResponseCode.SUCCESS);
+            } else {
+                responseBuilder.status(ResponseCode.NO_CONTENT);
+            }
+            return responseBuilder.data(result).build();
+        }catch (ParseException e){
+            e.printStackTrace();
+            responseBuilder.status(ResponseCode.INTERNAL_SERVER_ERROR);
+        }
         return responseBuilder.data(result).build();
+    }
+
+    boolean hasExceptionInList(Vacation general, List<Vacation> exceptionList){
+        for(Vacation exception: exceptionList){
+            if(general.getId() == exception.getParent_Id()){
+                return true;
+            }
+        }
+        return false;
     }
 }
--- mod-rest/src/main/java/com/mod/rest/repository/VacationRepository.java
+++ mod-rest/src/main/java/com/mod/rest/repository/VacationRepository.java
@@ -2,8 +2,11 @@ package com.mod.rest.repository;
 
 import com.mod.rest.model.Vacation;
 
+import java.util.List;
+
 /**
  * Created by omar.sabry on 12/9/2020.
  */
 public interface VacationRepository extends GenericRepository<Vacation,Long> {
+    List<Vacation> findByRule(String rule);
 }
